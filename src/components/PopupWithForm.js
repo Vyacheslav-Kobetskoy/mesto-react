7443efb0fb5b4541f574ea38afe498f1c52f1e4c
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2022-02-13 21:49:56 +0300

    feat:компонент PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,26 @@
-function PopupWithForm(props) {//title name children
-    return (
-        <article className={`popup popup_type_${props.name}`} aria-label="Попап">
-        <div className="popup__container">
-          <h2 className="popup__title">{props.title}</h2>
-          <form className="popup__form" name={`${props.name}Form`} noValidate>
+function PopupWithForm(props) {
+  return (
+    <article
+      className={`popup popup_type_${props.name} ${
+        props.isOpen ? "popup_opened" : ""
+      }`}
+      aria-label="Попап"
+    >
+      <div className="popup__container">
+        <h2 className={`popup__title popup__title_type_${props.name}`}>
+          {props.title}
+        </h2>
+        <form className="popup__form" name={`${props.name}Form`} noValidate>
+          <props.children />
+        </form>
+        <button
+          onClick={props.onClose}
+          className="popup__close-btn btn-hover popup__close-edit"
+          type="button"
+        ></button>
+      </div>
+    </article>
+  );
+}
 
-
-          
-          </form>
-          <button className="popup__close-btn btn-hover popup__close-edit" type="button"></button>
-        </div>
-      </article>
-
-    );
-  }
-  
-  export default PopupWithForm;
+export default PopupWithForm;
